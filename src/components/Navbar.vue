baa8b24ef1073f4c09da7424e4477f41d34d253c
Hennadii Panasiuk <gennapanasuk@gmail.com>
2026-06-03 12:48:41 +0200

    installed vue-router, added a few routes and added NavBar component changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import router from '@/router';
+
+</script>
+
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
         <div class="container py-2">
@@ -19,7 +24,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a href="my-tasks.html" class="nav-link">Tasks</a>
+                        <router-link :to="{ name: 'tasks' }" class="nav-link">Tasks</router-link>
                     </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link">Summary</a>
@@ -27,12 +32,10 @@
                 </ul>
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a href="#" class="btn btn-outline-secondary ms-2"
-                            >Login</a
-                        >
+                        <router-link :to="{ name: 'login' }" class="btn btn-outline-secondary ms-2">Login</router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="btn btn-danger ms-2">Register</a>
+                        <router-link :to="{ name: 'register' }" class="btn btn-danger ms-2">Register</router-link>
                     </li>
                     <li class="nav-item">
                         <a href="#" class="btn btn-outline-secondary ms-2"
